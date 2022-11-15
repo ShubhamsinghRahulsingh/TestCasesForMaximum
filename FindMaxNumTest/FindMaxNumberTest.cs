eb6b67a3909b8c096c7e0d1a263cdfe9c4597fbe
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-15 16:13:15 +0530

    (Shubham)Add: Finding maximum among three float numbers with test cases

--- FindMaxNumTest/FindMaxNumberTest.cs
+++ FindMaxNumTest/FindMaxNumberTest.cs
@@ -23,5 +23,23 @@ namespace FindMaxNumTest
             int result = maximum.FindMaxInteger(14, 17, 23); //AAA(Act,Arrange,Assert)
             Assert.AreEqual(23, result);
         }
+        [Test]
+        public void GivenMaxFirstNumFloat_WhenAnalysed_ShouldReturnFirstMax()//Max num at first position
+        {
+            float result = maximum.FindMaxFloat(23.0f, 17.0f, 14.0f); //AAA(Act,Arrange,Assert)
+            Assert.AreEqual(23.0, result);
+        }
+        [Test]
+        public void GivenMaxSecondNumFloat_WhenAnalysed_ShouldReturnSecondMax()//max number at second position
+        {
+            float result = maximum.FindMaxFloat(17.0f, 23.0f, 14.0f); //AAA(Act,Arrange,Assert)
+            Assert.AreEqual(23, result);
+        }
+        [Test]
+        public void GivenMaxThirdNumFloat_WhenAnalysed_ShouldReturnThirdMax()//max num at third position
+        {
+            float result = maximum.FindMaxFloat(14.0f, 17.0f, 23.0f); //AAA(Act,Arrange,Assert)
+            Assert.AreEqual(23, result);
+        }
     }
 }
--- FindMaximumNumber/FindMaxNum.cs
+++ FindMaximumNumber/FindMaxNum.cs
@@ -15,5 +15,18 @@
             else
                 return c;
         }
+        public float FindMaxFloat(float a, float b, float c)
+        {
+            if (a > b && a > c)
+            {
+                return a;
+            }
+            else if (b > a && b > c)
+            {
+                return b;
+            }
+            else
+                return c;
+        }
     }
 }

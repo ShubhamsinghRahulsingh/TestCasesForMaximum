cf2e61b9f86e4202b4e82152c64009b3529d4731
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-15 15:54:28 +0530

    (Shubham)Add: Finding maximum among three integers with test cases

--- FindMaxNumTest/FindMaxNumberTest.cs
+++ FindMaxNumTest/FindMaxNumberTest.cs
@@ -1,16 +1,27 @@
+using FindMaximumNumber;
+
 namespace FindMaxNumTest
 {
     public class FindMaxNumberTest
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
+        FindMaxNum maximum = new FindMaxNum();
         [Test]
-        public void Test1()
+        public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnFirstMax()//Max num at first position
         {
-            Assert.Pass();
+            int result = maximum.FindMaxInteger(23, 17, 14); //AAA(Act,Arrange,Assert)
+            Assert.AreEqual(23,result);
+        }
+        [Test]
+        public void GivenMaxSecondNum_WhenAnalysed_ShouldReturnSecondMax()//max number at second position
+        {
+            int result = maximum.FindMaxInteger(17, 23, 14); //AAA(Act,Arrange,Assert)
+            Assert.AreEqual(23, result);
+        }
+        [Test]
+        public void GivenMaxThirdNum_WhenAnalysed_ShouldReturnThirdMax()//max num at third position
+        {
+            int result = maximum.FindMaxInteger(14, 17, 23); //AAA(Act,Arrange,Assert)
+            Assert.AreEqual(23, result);
         }
     }
 }
--- FindMaximumNumber/FindMaxNum.cs
+++ FindMaximumNumber/FindMaxNum.cs
@@ -1,7 +1,19 @@
 ﻿namespace FindMaximumNumber
 {
-    public class Class1
+    public class FindMaxNum
     {
-
+        public int FindMaxInteger(int a, int b, int c)
+        {
+            if (a > b && a > c)
+            {
+                return a;
+            }
+            else if (b > a && b > c)
+            {
+                return b;
+            }
+            else
+                return c;
+        }
     }
 }

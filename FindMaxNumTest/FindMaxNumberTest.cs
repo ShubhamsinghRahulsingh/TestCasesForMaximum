36f1692d4154530a974b97252905b0eaae1e2b63
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-15 16:39:39 +0530

    (Shubham)Add: Finding maximum among three string with test cases

--- FindMaxNumTest/FindMaxNumberTest.cs
+++ FindMaxNumTest/FindMaxNumberTest.cs
@@ -33,13 +33,31 @@ namespace FindMaxNumTest
         public void GivenMaxSecondNumFloat_WhenAnalysed_ShouldReturnSecondMax()//max number at second position
         {
             float result = maximum.FindMaxFloat(17.0f, 23.0f, 14.0f); //AAA(Act,Arrange,Assert)
-            Assert.AreEqual(23, result);
+            Assert.AreEqual(23.0, result);
         }
         [Test]
         public void GivenMaxThirdNumFloat_WhenAnalysed_ShouldReturnThirdMax()//max num at third position
         {
             float result = maximum.FindMaxFloat(14.0f, 17.0f, 23.0f); //AAA(Act,Arrange,Assert)
-            Assert.AreEqual(23, result);
+            Assert.AreEqual(23.0, result);
+        }
+        [Test]
+        public void GivenMaxFirstNumstring_WhenAnalysed_ShouldReturnFirstMax()//Max string at first position
+        {
+            string result = maximum.FindMaxString("Peach","Banana","Apple"); //AAA(Act,Arrange,Assert)
+            Assert.AreEqual("Peach", result);
+        }
+        [Test]
+        public void GivenMaxSecondNumString_WhenAnalysed_ShouldReturnSecondMax()//max string at second position
+        {
+            string result = maximum.FindMaxString("Banana", "Peach", "Apple"); //AAA(Act,Arrange,Assert)
+            Assert.AreEqual("Peach", result);
+        }
+        [Test]
+        public void GivenMaxThirdNumString_WhenAnalysed_ShouldReturnThirdMax()//max string at third position
+        {
+            string result = maximum.FindMaxString("Banana", "Apple", "Peach"); //AAA(Act,Arrange,Assert)
+            Assert.AreEqual("Peach", result);
         }
     }
 }
